5f847fca452a3caed9b08281245b8e64f87e7afd
Mr.yao <Mr.yao.email@qq.com>
2023-09-11 11:38:45 +0800

    feat: 优化demo-c的提示

--- scripts/create-demo-file.sh
+++ scripts/create-demo-file.sh
@@ -2,24 +2,24 @@
 
 current_dir=$(pwd)
 father_dir=$(basename "$PWD")
-echo $father_dir  #输出 demo
-echo "$current_dir"  #输出 demo
+# echo $father_dir  #输出 demo
+# echo "$current_dir"  #输出 demo
 
 [[ $1 ]] && FILE_TYPE="$1" || FILE_TYPE="$father_dir"
 
-echo FILE_TYPE:$FILE_TYPE
+# echo FILE_TYPE:$FILE_TYPE
 
 cd $(pwd)
 filename=`date +%m-%d`
 count=$(ls -1 $filename*.$FILE_TYPE 2>/dev/null | wc -l | tr -d '[:space:]')
 
-echo count: $count
-
 if [ $count == 0 ]; then
-    touch  "$filename.$FILE_TYPE"
+    newfile="$filename.$FILE_TYPE"
 else
-    touch  "$filename-$count.$FILE_TYPE"
+    newfile="$filename-$count.$FILE_TYPE"
 fi
 
+echo newfile: $newfile
 
+touch $newfile
 
